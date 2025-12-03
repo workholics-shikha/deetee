85685dd45d65aa935bee07ba09dffae027f1eb50
workholics-shikha <workholics.shikha@gmail.com>
2025-12-03 14:46:58 +0530

    Updated operations logic to improve performance and fix bugs

--- app/Http/Controllers/OperationsController.php
+++ app/Http/Controllers/OperationsController.php
@@ -11,30 +11,6 @@ class OperationsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index_old(Request $request)
-    {
-
-        $search      = $request->input('search');
-        $operations  = OperationMaster::with('machines')
-
-            ->when($search, function ($query, $search) {
-                $query->where('operation_name', 'like', "%$search%");
-                $query->orWhere('unit', 'like', "%$search%");
-                $query->orWhere('matrix', 'like', "%$search%");
-            })
-            ->paginate(PAGE_NO);
-
-        $container['records'] = $operations;
-        $container['totalrecords'] = $operations->total();
-        if ($request->get('page')) {
-            $html = view('admin/snippets/operations', $container)->render();
-            $container['html'] = $html;
-            $container['pagination'] = (string) $operations->links();
-            return response()->json($container);
-        }
-        return view('operations.list', $container);
-    }
- 
     public function index(Request $request)
     {
         $search = $request->input('search');
